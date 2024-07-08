a1e788e155fe86d023365ca698849c961bd82249
Preston Sharpe <psharpe@xentity.com>
2024-07-08 12:49:08 -0600

    add inter modle for points and add rankings for standings

---
+++ models/purty/standings.sql
@@ -0,0 +1,17 @@
+SELECT games_agg.team_id
+, teams.team_name
+, conf.conference
+, games_agg.num_home_games + games_agg.num_away_games AS games_played
+, games_agg.points_home
+, games_agg.points_away
+, games_agg.points_home + games_agg.points_away AS total_points
+, total_points / games_played AS points_per_game
+, ROW_NUMBER() OVER(ORDER BY TOTAL_POINTS DESC) AS shield_standings
+, ROW_NUMBER() OVER(PARTITION BY conference ORDER BY TOTAL_POINTS DESC) AS conference_standings
+, ROW_NUMBER() OVER(ORDER BY points_per_game DESC) AS shield_ppg_standings
+, ROW_NUMBER() OVER(PARTITION BY conference ORDER BY points_per_game DESC) AS conference_ppg_standings
+FROM {{ ref('home_away_agg') }} games_agg
+LEFT JOIN {{ source('raw', 'teams') }} teams
+ON games_agg.team_id = teams.team_id
+LEFT JOIN {{ source('raw', 'team_conferences') }} conf
+ON teams.team_name = conf.team_name
