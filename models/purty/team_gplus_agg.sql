7e5d921e448eda7250f8ac4aff149a604e40c599
Preston Sharpe <psharpe@xentity.com>
2024-07-20 12:11:18 -0600

    added team gplus models

---
+++ models/purty/team_gplus_agg.sql
@@ -0,0 +1,24 @@
+with gplus_agg AS (
+	SELECT team_id
+	, SUM(goals_added_for) AS total_gplus
+	FROM {{ ref('team_gplus') }}
+	GROUP BY 1
+) SELECT 
+teams.team_id
+, teams.team_name
+, tc.conference
+, stand.games_played 
+, tg.total_gplus
+, tg.total_gplus / stand.games_played as avg_gplus_per_game
+, ROW_NUMBER() OVER(ORDER BY total_gplus DESC) AS team_gplus_rank
+, ROW_NUMBER() OVER(PARTITION BY tc.conference ORDER BY total_gplus DESC) AS team_gplus_conf_rank
+, ROW_NUMBER() OVER(ORDER BY avg_gplus_per_game DESC) AS avg_gplus_rank
+, ROW_NUMBER() OVER(PARTITION BY tc.conference ORDER BY avg_gplus_per_game DESC) AS avg_gplus_conf_rank
+FROM gplus_agg tg
+LEFT JOIN {{ source('raw', 'teams') }} teams
+ON tg.team_id = teams.team_id
+LEFT JOIN {{ source('raw', 'team_conferences') }} tc
+ON teams.team_name = tc.team_name
+LEFT JOIN {{ ref('standings') }} stand
+ON tg.team_id = stand.team_id
+ORDER BY avg_gplus_per_game DESC
