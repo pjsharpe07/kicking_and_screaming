aeb1a5c3aef291eda357aa95433d89772bcd35b0
Preston Sharpe <psharpe@xentity.com>
2024-07-21 12:24:00 -0600

    add intermediate model for teams w/conference info

--- models/purty/team_gplus_agg.sql
+++ models/purty/team_gplus_agg.sql
@@ -7,19 +7,17 @@ with gplus_agg AS (
 ) SELECT 
 teams.team_id
 , teams.team_name
-, tc.conference
+, teams.conference
 , stand.games_played 
 , tg.total_gplus
 , tg.total_gplus / stand.games_played as avg_gplus_per_game
 , ROW_NUMBER() OVER(ORDER BY total_gplus DESC) AS team_gplus_rank
-, ROW_NUMBER() OVER(PARTITION BY tc.conference ORDER BY total_gplus DESC) AS team_gplus_conf_rank
+, ROW_NUMBER() OVER(PARTITION BY teams.conference ORDER BY total_gplus DESC) AS team_gplus_conf_rank
 , ROW_NUMBER() OVER(ORDER BY avg_gplus_per_game DESC) AS avg_gplus_rank
-, ROW_NUMBER() OVER(PARTITION BY tc.conference ORDER BY avg_gplus_per_game DESC) AS avg_gplus_conf_rank
+, ROW_NUMBER() OVER(PARTITION BY teams.conference ORDER BY avg_gplus_per_game DESC) AS avg_gplus_conf_rank
 FROM gplus_agg tg
-LEFT JOIN {{ source('raw', 'teams') }} teams
+LEFT JOIN {{ ref('teams_clean') }} teams
 ON tg.team_id = teams.team_id
-LEFT JOIN {{ source('raw', 'team_conferences') }} tc
-ON teams.team_name = tc.team_name
 LEFT JOIN {{ ref('standings') }} stand
 ON tg.team_id = stand.team_id
 ORDER BY avg_gplus_per_game DESC
