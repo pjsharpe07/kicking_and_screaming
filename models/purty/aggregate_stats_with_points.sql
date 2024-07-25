6f48f2a0dcc9b992fe729c2a9b969a476ad9f828
Preston Sharpe <psharpe@xentity.com>
2024-07-25 06:45:42 -0600

    aggregate stats with points model

---
+++ models/purty/aggregate_stats_with_points.sql
@@ -0,0 +1,15 @@
+SELECT stand.team_id
+, stand.team_name
+, stand.conference 
+, stand.total_points
+, stand.points_per_game
+, xg.xgoals_for
+, xg.xgoals_against
+, xg.xgoal_difference
+, gplus.total_gplus
+, gplus.avg_gplus_per_game
+FROM {{ ref('standings') }} stand
+LEFT JOIN {{ ref('team_xg_agg') }} xg
+ON stand.team_id = xg.team_id
+LEFT JOIN {{ ref('team_gplus_agg') }} gplus
+ON stand.team_id = gplus.team_id
