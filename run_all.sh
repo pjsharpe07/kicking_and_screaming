f658edfd59aeb387e81d8f9ce6b7d1140efaa76f
Preston Sharpe <psharpe@xentity.com>
2024-08-24 18:02:05 -0600

    do not recreate venv each iteration

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,14 @@
 echo "Creating our virtual environment" &&
-rm -rf .venv &&
-python -m venv .venv &&
+# 1. instantiate venv and install any changed dependencies
 source .venv/Scripts/activate &&
 echo "Installing dependencies" &&
-pip install -q -r requirements.txt &&
+pip install -q  -r requirements.txt --upgrade &&
+# 2. fetch data from asa apis 
 echo "Beginning to fetch data" &&
 python -u raw_data_etl/fetch_game_data.py &&
 python -u raw_data_etl/fetch_team_data.py &&
 python -u raw_data_etl/fetch_player_data.py &&
+# build our models via dbt
 echo "Beginning to build our data model" &&
 dbt build
 $SHELL
