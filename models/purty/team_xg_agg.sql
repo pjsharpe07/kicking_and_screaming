90a1833320c5aa5260c79c8e9cc05ce2834cc343
Preston Sharpe <psharpe@xentity.com>
2024-11-20 15:55:23 -0700

    adding 2 snapshots in dbt, bump reqs, bugfix for season_name in join clause

--- models/purty/team_xg_agg.sql
+++ models/purty/team_xg_agg.sql
@@ -3,6 +3,7 @@ xg.team_id
 , teams.team_name
 , teams.conference
 , xg.season_name
+, stand.games_played
 , xg.shots_for
 , xg.shots_against
 , xg.goals_for
@@ -11,13 +12,16 @@ xg.team_id
 , xg.xgoals_for
 , xg.xgoals_against
 , xg.xgoal_difference
-, ROW_NUMBER() OVER(PARTITION BY season_name ORDER BY xgoals_for DESC) AS xgoals_for_rank
-, ROW_NUMBER() OVER(PARTITION BY conference, season_name ORDER BY xgoals_for DESC) AS xgoals_for_conf_rank
-, ROW_NUMBER() OVER(PARTITION BY season_name ORDER BY xgoals_against DESC) AS xgoals_against_rank
-, ROW_NUMBER() OVER(PARTITION BY conference, season_name ORDER BY xgoals_against DESC) AS xgoals_against_conf_rank
-, ROW_NUMBER() OVER(PARTITION BY season_name ORDER BY xgoal_difference DESC) AS xgoal_diff_rank
-, ROW_NUMBER() OVER(PARTITION BY conference, season_name ORDER BY xgoal_difference DESC) AS xgoal_diff_conf_rank
+, ROW_NUMBER() OVER(PARTITION BY xg.season_name ORDER BY xgoals_for DESC) AS xgoals_for_rank
+, ROW_NUMBER() OVER(PARTITION BY teams.conference, xg.season_name ORDER BY xgoals_for DESC) AS xgoals_for_conf_rank
+, ROW_NUMBER() OVER(PARTITION BY xg.season_name ORDER BY xgoals_against DESC) AS xgoals_against_rank
+, ROW_NUMBER() OVER(PARTITION BY teams.conference, xg.season_name ORDER BY xgoals_against DESC) AS xgoals_against_conf_rank
+, ROW_NUMBER() OVER(PARTITION BY xg.season_name ORDER BY xgoal_difference DESC) AS xgoal_diff_rank
+, ROW_NUMBER() OVER(PARTITION BY teams.conference, xg.season_name ORDER BY xgoal_difference DESC) AS xgoal_diff_conf_rank
 FROM {{ source('raw', 'team_xg')  }} xg
 LEFT JOIN {{ ref('teams_clean') }} teams
-ON xg.team_id = teams.team_id
+    ON xg.team_id = teams.team_id
+LEFT JOIN {{ ref('standings') }} stand
+    ON xg.team_id = stand.team_id
+    AND xg.season_name = stand.season_name
 ORDER BY xgoal_difference DESC
---
+++ snapshots/fact_models/team_xg_agg_snapshot.sql
@@ -0,0 +1,29 @@
+{% snapshot team_xg_agg_snapshot %}
+
+    {{
+        config(
+          unique_key="team_id || '-' || season_name",
+          strategy="check",
+          check_cols=['games_played'],
+          target_schema="snapshot"
+        )
+    }}
+
+select
+    team_id
+  , team_name
+  , season_name
+  , conference
+  , games_played
+  , games_played
+  , shots_for
+  , shots_against
+  , goals_for
+  , goals_against
+  , goal_difference
+  , xgoals_for
+  , xgoals_against
+  , xgoal_difference
+from {{ ref("team_xg_agg") }}
+
+{% endsnapshot %}
