5ddbf01f4fa11f967de9ead02e24a09c64867e77
Preston Sharpe <psharpe@xentity.com>
2026-03-21 19:59:55 -0600

    formatting

--- raw_data_etl/fetch_game_data.py
+++ raw_data_etl/fetch_game_data.py
@@ -50,10 +50,12 @@ game_data = asa_client.get_games(leagues="mls", seasons=season_name)
 
 # these are nonsense columns that they are providing
 try:
-    columns_to_drop = ['home_penalties', 'away_penalties']
+    columns_to_drop = ["home_penalties", "away_penalties"]
     game_data.drop(columns=columns_to_drop, inplace=True)
 except:
-    print(f"[games_etl] Unable to drop {', '.join(columns_to_drop)} columns. Maybe they don't exist anymore?")
+    print(
+        f"[games_etl] Unable to drop {', '.join(columns_to_drop)} columns. Maybe they don't exist anymore?"
+    )
 
 game_data = validate_source_and_db_columns(
     source_df=game_data, target_table="games", cursor=cursor
